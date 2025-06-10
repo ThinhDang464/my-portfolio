de455ba4a2c4cf38315cd1f975b90a0818060dea
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-06-10 20:27:17 +1000

    SKill and Hero components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { GameSection } from "@/components/GameSection";
+import { Skills } from "@/components/Skills";
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <Skills />
       <GameSection />
     </div>
   );
---
+++ src/components/Skills.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function Skills() {
+  return (
+    <section className="flex items-center justify-center px-20 py-12 ">
+      <div className="bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 rounded-lg p-8 max-w-4x1 w-full"></div>
+    </section>
+  );
+}
